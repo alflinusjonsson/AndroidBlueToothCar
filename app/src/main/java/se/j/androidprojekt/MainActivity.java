f7662cf240433159a3da292514658ce00d05975a
kaan17ak <kaan17ak@student.ju.se>
2019-02-12 12:03:13 +0100

    Added functions for the arrows, and background to the radar

--- app/src/main/java/se/j/androidprojekt/MainActivity.java
+++ app/src/main/java/se/j/androidprojekt/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     RadarView mRadarView = null;
@@ -18,8 +21,82 @@ public class MainActivity extends AppCompatActivity {
         if (mRadarView != null) mRadarView.startAnimation();
     }
 
-    public void SettingsButtonClicked(View view){
-        //Intent intent = new Intent(this, SettingsActivity.class);
-        //startActivity(intent);
+    public void SettingsButtonClicked(View view) {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
+
+    public void ArrowUpPressed(View view) {
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        //Run motor forward
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        //Stop motor
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+    public void ArrowLeftPressed(View view) {
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        //Run motor left
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        //Stop motor
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void ArrowRightPressed(View view) {
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        //Run motor right
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        //Stop motor
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void ArrowDownPressed(View view) {
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        //Run motor backwards
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        //Stop motor
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
 }
+
+
+
+
+
+
