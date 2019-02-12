0a582bd6b99c636b82be2370cd286b74aa40eb13
kaan17ak <kaan17ak@student.ju.se>
2019-02-12 13:38:01 +0100

    Added speed and distance parts

--- app/src/main/java/se/j/androidprojekt/MainActivity.java
+++ app/src/main/java/se/j/androidprojekt/MainActivity.java
@@ -7,16 +7,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     RadarView mRadarView = null;
-
+    int speedOutput = 50;
+    int distanceFront = 10;
+    int distanceBack = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        TextView speedTextView = (TextView) findViewById(R.id.speed);
+        speedTextView.setText(speedOutput + "\n" + "km/h");
+
+        TextView disFront = (TextView) findViewById(R.id.disFront);
+        disFront.setText(distanceFront + "\n" + "cm");
+
+        TextView disBack = (TextView) findViewById(R.id.disBack);
+        disBack.setText(distanceBack + "\n" + "cm");
+
         mRadarView = (RadarView) findViewById(R.id.radarView);
+
         mRadarView.setShowCircles(true);
         if (mRadarView != null) mRadarView.startAnimation();
     }
@@ -42,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void ArrowLeftPressed(View view) {
         view.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -92,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
 
 
