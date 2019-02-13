ee2ec4c67cc2664c223e72999eea9ad69f51d8ae
Linus Jönsson <alflinusjonsson@gmail.com>
2019-02-13 13:07:21 +0100

    Changed bluetooth AGAIN://

--- app/src/main/java/se/j/androidprojekt/BlueTooth.java
+++ app/src/main/java/se/j/androidprojekt/BlueTooth.java
@@ -1,7 +1,6 @@
 package se.j.androidprojekt;
 
 import android.app.Activity;
-import android.bluetooth.BluetoothClass;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +10,7 @@ import android.widget.Toast;
 
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
 import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
 
 public class BlueTooth extends AppCompatActivity {
 
@@ -23,7 +23,9 @@ public class BlueTooth extends AppCompatActivity {
         setContentView(R.layout.bluetooth);
 
         bluetooth = new BluetoothSPP(this);
+
         connect = (Button) findViewById(R.id.connect);
+
 
         if (!bluetooth.isBluetoothAvailable()) {
             Toast.makeText(getApplicationContext(), "Bluetooth is not available", Toast.LENGTH_SHORT).show();
@@ -50,28 +52,32 @@ public class BlueTooth extends AppCompatActivity {
                 if (bluetooth.getServiceState() == BluetoothState.STATE_CONNECTED) {
                     bluetooth.disconnect();
                 } else {
-                    Intent intent = new Intent(getApplicationContext(), BluetoothClass.Device.class);
+                    Intent intent = new Intent(getApplicationContext(), DeviceList.class);
                     startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
                 }
             }
         });
+
+
     }
-        public void onStart() {
-            super.onStart();
-            if (!bluetooth.isBluetoothEnabled()) {
-                bluetooth.enable();
-            } else {
-                if (!bluetooth.isServiceAvailable()) {
-                    bluetooth.setupService();
-                    bluetooth.startService(BluetoothState.DEVICE_OTHER);
-                }
+
+    public void onStart() {
+        super.onStart();
+        if (!bluetooth.isBluetoothEnabled()) {
+            bluetooth.enable();
+        } else {
+            if (!bluetooth.isServiceAvailable()) {
+                bluetooth.setupService();
+                bluetooth.startService(BluetoothState.DEVICE_OTHER);
             }
         }
+    }
 
-        public void onDestroy() {
-            super.onDestroy();
-            bluetooth.stopService();
-        }
+
+    public void onDestroy() {
+        super.onDestroy();
+        bluetooth.stopService();
+    }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
